310f7eed6ceec4dca150a6858f74fb7a37404db4
Florian Pallas <mail@fpallas.com>
2024-05-24 17:25:37 +0200

    switch from radio buttons to buttons

--- client/src/lib/prompts/SelectPrompt.svelte
+++ client/src/lib/prompts/SelectPrompt.svelte
@@ -11,20 +11,14 @@
 <form
   on:submit={(event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    dispatch("submit", data.get("answer"));
   }}
 >
-  <fieldset>
-    <legend>{text}</legend>
-    <div>
-      {#each Object.entries(options) as [value, label]}
-        <input type="radio" id={value} name="answer" {value} />
-        <label for={value}>{label}</label>
-      {/each}
-    </div>
-    <div>
-      <button type="submit">Submit</button>
-    </div>
-  </fieldset>
+  <div class="vote-box">
+    <p>{text}</p>
+    {#each Object.entries(options) as [value, label]}
+      <button type="button" on:click={() => dispatch("submit", value)}>
+        {label}</button
+      >
+    {/each}
+  </div>
 </form>

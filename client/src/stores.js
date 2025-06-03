0a8189fc25e7d6667768caae3c6fffca8e65b659
Florian Pallas <mail@fpallas.com>
2025-06-03 16:15:56 +0200

    chore: remove unused console log

--- client/src/stores.js
+++ client/src/stores.js
@@ -4,7 +4,6 @@ import { readable } from "svelte/store";
  * @type {import("svelte/store").Readable<{questions: string[]}>}
  */
 export const content = readable({ questions: [] }, function start(set) {
-  console.log("x", window);
   fetch(`content/${window.APP_LOCALE}/content.json`)
     .then((response) => response.json())
     .then((data) => set(data));

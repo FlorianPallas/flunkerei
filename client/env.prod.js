2a85476d6c1acbc9c36c10b6daeee50a10ee4939
Florian Pallas <mail@fpallas.com>
2025-06-03 15:31:58 +0200

    refactor: fix api base url

--- client/env.prod.js
+++ client/env.prod.js
@@ -1,3 +1,3 @@
 window.APP_BASE_URL = "https://flunkerei.fpallas.com";
-window.APP_API_BASE_URL = "https://flunkerei.fpallas.com";
+window.APP_API_BASE_URL = "https://flunkerei-api.fpallas.com";
 window.APP_LOCALE = "de";
